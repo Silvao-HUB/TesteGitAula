8c15c38c386096d13a60aa36d03b6693534a6965
Silvio <silvio.rjunior4@senacsp.edu.br>
2025-01-28 19:57:34 -0300

    Alteração no arquivo do exercćio 5 - Alterado OK

--- Exercícios FÉRIAS/exercicio1.js
+++ Exercícios FÉRIAS/exercicio1.js
@@ -4,5 +4,5 @@ let soma = parseFloat(prompt('Informe o primeiro número: '));
 let soma1 = parseFloat(prompt('Informe o segundo número: '));
 
 var resultado= (soma + soma1);
-console.log('O resultado da sua conta é: ', resultado)
+console.log('O resultado da sua conta  salva é: ', resultado)
 
