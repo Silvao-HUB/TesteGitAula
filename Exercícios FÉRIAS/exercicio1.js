77e7ea8129aab9542fdfdb2d8369a582d3bc7bab
Silvio <silvio.rjunior4@senacsp.edu.br>
2025-01-28 21:22:50 -0300

    corrigindo mensagem cálculo exercício1

--- Exercícios FÉRIAS/exercicio1.js
+++ Exercícios FÉRIAS/exercicio1.js
@@ -1,7 +1,7 @@
 const prompt =require('prompt-sync')();
 
-let soma = parseFloat(prompt('Informe o primeiro número: '));
-let soma1 = parseFloat(prompt('Informe o segundo número: '));
+let soma = parseFloat(prompt('Informe o primeiro número para somar: '));
+let soma1 = parseFloat(prompt('Informe o segundo número para somar: '));
 
 var resultado= (soma + soma1);
 console.log('O resultado da sua conta  salva é: ', resultado)
