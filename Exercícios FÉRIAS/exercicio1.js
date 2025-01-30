31327eeb30270a85bac1737fba3e9a5405daf3ce
Silvio Rocha <silvao.vey@gmail.com>
2025-01-30 20:19:25 -0300

    Alteração de mensagem no prompt

--- Exercícios FÉRIAS/exercicio1.js
+++ Exercícios FÉRIAS/exercicio1.js
@@ -1,7 +1,7 @@
 const prompt =require('prompt-sync')();
 
-let soma = parseFloat(prompt('Informe o primeiro número para somar: '));
-let soma1 = parseFloat(prompt('Informe o segundo número para somar: '));
+let soma = parseFloat(prompt('Informe o primeiro número para podermos somar: '));
+let soma1 = parseFloat(prompt('Informe o segundo número para  podermos somar: '));
 
 var resultado= (soma + soma1);
 console.log('O resultado da sua conta  salva é: ', resultado)
